70839d48793170004bd2c425750aba2015c05317
emartinez <enri.fc.98@gmail.com>
2022-09-26 18:04:36 +0200

    ADD: ft_strlcpy

--- Makefile
+++ Makefile
@@ -15,6 +15,7 @@ SRC = ft_bzero.c\
 	   ft_memcpy.c\
 	   ft_memset.c\
 	   ft_strchr.c\
+	   ft_strlcpy.c\
 	   ft_strlen.c\
 	   ft_strrchr.c\
 	   ft_tolower.c\
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 16:21:10 by franmart          #+#    #+#             */
-/*   Updated: 2022/09/26 15:59:02 by franmart         ###   ########.fr       */
+/*   Updated: 2022/09/26 17:54:47 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int		ft_isprint(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 int		ft_tolower(int c);
