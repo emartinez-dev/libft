d67d9fff8b6ce719e2bc2e15e364d080a57ef66e
emartinez <enri.fc.98@gmail.com>
2022-10-20 19:56:02 +0200

    FIX: FINALLY FT_SPLIT

--- ft_split.c
+++ ft_split.c
@@ -6,34 +6,30 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:49:10 by franmart          #+#    #+#             */
-/*   Updated: 2022/09/29 19:45:38 by franmart         ###   ########.fr       */
+/*   Updated: 2022/10/20 19:53:42 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	ft_n_delimits(char const *s, char c)
+unsigned int	ft_word_count(char const *s, char c)
 {
-	int	i;
-	int	j;
-	int	dels;
+	unsigned int	count;
+	int				i;
 
 	i = 0;
-	j = 0;
-	dels = 0;
+	count = 0;
+	if (!s || !c)
+		return (0);
+	while (s[i] == c)
+		i++;
 	while (s[i] != '\0')
 	{
-		j = i;
-		if (s[j] == c)
-		{
-			while (s[j] == c)
-				j++;
-			dels++;
-			i = j;
-		}
+		if (s[i] == c && s[i + 1] != '\0' && s[i + 1] != c)
+			count++;
 		i++;
 	}
-	return (dels);
+	return (count + 1);
 }
 
 unsigned int	ft_next_del_pos(char const *s, char c)
@@ -61,33 +57,37 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	j = 0;
-	next_del = ft_next_del_pos(s, c);
-	arr_of_str = malloc((ft_n_delimits(s, c) + 2) * sizeof(char *));
+	arr_of_str = malloc((ft_word_count(s, c) + 1) * sizeof(char *));
 	if (!arr_of_str)
 		return (0);
+	next_del = ft_next_del_pos(s, c);
 	while (i < ft_strlen(s))
 	{
 		if (next_del > 0)
 			arr_of_str[j++] = ft_substr(s, i, next_del);
 		i += next_del + 1;
-		next_del = ft_next_del_pos(s + i, c);
+		if (i < ft_strlen(s))
+			next_del = ft_next_del_pos(s + i, c);
 	}
 	arr_of_str[j] = 0;
 	return (arr_of_str);
 }
+
 /*
 int	main(void)
 {
-	char *str = "a,b,c,d,,,,e,f,g,h,i,j,k";
+	char *str = NULL;
 	char **arr;
 	int	j;
 
 	j = 0;
-	arr = ft_split(str, ',');
+	arr = ft_split(str, 0);
+	
 	while (arr[j] != 0)
 	{
 		printf("%s", arr[j]);
 		j++;
 	}
 	return (0);
-}*/
+}
+*/
